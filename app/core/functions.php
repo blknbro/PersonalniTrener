e64457ebfc2ff90dd6502855e1af7728f4902869
blknbro <duledule17113@gmail.com>
2024-05-26 22:41:39 +0200

    mailer

--- app/core/functions.php
+++ app/core/functions.php
@@ -18,3 +18,28 @@ function createToken(int $length): ?string
         return null;
     }
 }
+
+function sendMail(){
+//    $mail = new PHPMailer(true);
+//
+//    try {
+//        $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+//        $mail->isSMTP();
+//        $mail->Host = 'mail.nm.stud.vts.su.ac.rs';
+//        $mail->SMTPAuth = true;
+//        $mail->Username = 'nm';
+//        $mail->Password = 'u76tVYucyaaf3lI';
+//        $mail->SMTPSecure = 'tls';
+//        $mail->Port = 587;
+//        $mail->setFrom('nm@nm.stud.vts.su.ac.rs', 'Webmaster');
+//        $mail->addAddress('duledule17113@gmail.com');
+//        $mail->isHTML(true);
+//        $mail->Subject = 'Neuspeh';
+//        $mail->Body = '<h1>Došlo do još jednog neuspešnog prijavljivanja.</h1>';
+//        $mail->AltBody = 'Došlo do još jednog neuspešnog prijavljivanja.';
+//        $mail->send();
+//        header("Location:index.php?error=Invalid user!");
+//    } catch (Exception $e) {
+//        echo "Message could not be sent. Mailer error {$mail->ErrorInfo}";
+//    }
+}
